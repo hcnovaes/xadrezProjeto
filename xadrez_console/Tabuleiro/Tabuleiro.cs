3ea731c1a4d55612b03a41cfe260ee679488f28f
Henrique Novaes <novaeshenrique@outlook.com>
2020-09-14 12:25:05 -0300

    começando a implementar um metodo que imprime o tabuleiro

---
+++ xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+    }
+}
+
