aa91f1cd1eefcf1612ffe7bdabc7c41b0ade368f
Henrique Novaes <novaeshenrique@outlook.com>
2020-09-16 10:23:15 -0300

    Implementando fundo de movimentos possíveis

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -21,6 +21,14 @@ namespace xadrez_console
                     Console.WriteLine("");
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoCXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine("");
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoCXadrez().toPosicao();
 
